10c53197262305570ba2cff1a901bc1042affb23
eelizabethriley <eeriley@smith.edu>
2022-12-21 23:38:28 -0500

    Javadoc comments

--- Room.java
+++ Room.java
@@ -1,5 +1,5 @@
 
-/** A class Location
+/** A class Location with a name, description, booleans to indicate if a path exists in a given direction, and strings recording which room exists in that direction if a path exists.
  * 
  */
 public class Room {
@@ -21,7 +21,7 @@ public class Room {
     String exitSouth;
 
 
-    /* Default constructor for a location of unspecified name, description, and exits. */
+    /** Default constructor for a location of unspecified name, description, and exits. */
     public Room() {
         this("<Undefined Name", "<Empty Description>", false, false, false, false, "N1", "E1", "W1", "S1");
     }
@@ -32,16 +32,16 @@ public class Room {
 
     /**
     /* Complete constructor for a location of specified name, description, and exits.
-    * @param name
-     * @param description
-     * @param isNorth
-     * @param isEast
-     * @param isWest
-     * @param isSouth
-     * @param exitNorth
-     * @param exitEast
-     * @param exitWest
-     * @param exitSouth
+    * @param name name of this room
+     * @param description description of this room with details about the physical environment as well as hints about which room can be found in each direction.
+     * @param isNorth true when there is a path north of this room
+     * @param isEast  true when there is a path east of this room
+     * @param isWest  true when there is a path west of this room
+     * @param isSouth  true when there is a path south of this room
+     * @param exitNorth the String of format "N1", "S2", etc. indicating which room in the map is found north of the room
+     * @param exitEast the String of format "N1", "S2", etc. indicating which room in the map is found east of the room
+     * @param exitWest the String of format "N1", "S2", etc. indicating which room in the map is found west of the room
+     * @param exitSouth the String of format "N1", "S2", etc. indicating which room in the map is found south of the room
      */
     public Room (String name, String description, boolean isNorth, boolean isEast, boolean isWest, boolean isSouth, String exitNorth, String exitEast, String exitWest, String exitSouth) {
         // Constructor for a Location
@@ -58,20 +58,26 @@ public class Room {
     }
 
     //         ** Accessor methods **
-    // location name
+    /**
+     * Accessor for Room name
+     * @return name of this room
+     */
     public String getName() {
         return name;
     }
 
-    // location description
+    /**
+     * Accessor for room description
+     * @return description of this room
+     */
     public String getDesc() {
         return description;
     }
 
     /**
-    /* Accessor for different direction typ booleanss, indicating whether or not the player can move in this direction. 
-    * @param direction
-    * @return the location 
+    /* Accessor for different direction booleans, indicating whether or not the player can move in this direction. 
+    * @param direction the direction we want to check for an exit path
+    * @return whether or not a path exists in the given direction
     */
     public boolean isExit(String direction) {
         switch(direction) {
